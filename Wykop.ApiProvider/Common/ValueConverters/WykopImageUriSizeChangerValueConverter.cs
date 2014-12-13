f2b9d34c5e8fda966f86c336652e70640877c4be
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-13 19:21:34 +0100

    Added value converter which gives option to adjust image width/height for wykop image source uri.

---
+++ Wykop.ApiProvider/Common/ValueConverters/WykopImageUriSizeChangerValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Windows.UI.Xaml.Data;
+
+namespace Wykop.ApiProvider.Common.ValueConverters
+{
+    public class WykopImageUriSizeChangerValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var imageUri = (Uri) value;
+            string regexPatternToReplace = @",w\d*h\d*";
+            string replacementString = String.Empty;
+
+            if (RequestedWidth > 0 && RequestedHeight > 0)
+                replacementString = ",w" + RequestedWidth + "h" + RequestedHeight;
+
+            string fullSizeImageUri = Regex.Replace(imageUri.OriginalString, regexPatternToReplace, replacementString);
+            return new Uri(fullSizeImageUri);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RequestedWidth { get; set; }
+        public int RequestedHeight { get; set; }
+    }
+}
