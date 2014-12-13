368a4d0c4f18fa8330f96f031d96585134e6ddad
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-13 14:34:43 +0100

    Implemented UpcomingMainLinksRequest unit tests.
    Moved empty post body check assertion to extension method - RestRequestAsserts.

---
+++ Wykop.ApiProvider.UnitTests/Common/RestRequestAsserts.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Net.Http;
+using NUnit.Framework;
+using RestSharp.Portable;
+
+namespace Wykop.ApiProvider.UnitTests.Common
+{
+    public static class RestRequestAsserts
+    {
+        public static void AssertThatRequestHasEmptyPostBody(this RestRequest restRequest)
+        {
+            var containsPostParameters = restRequest.Parameters.Any(x =>
+                (restRequest.Method == HttpMethod.Post && x.Type == ParameterType.GetOrPost) ||
+                x.Type == ParameterType.RequestBody);
+
+            Assert.IsFalse(containsPostParameters, "Request contains post parameters");
+        }
+    }
+}
--- Wykop.ApiProvider.UnitTests/Data/Link/Links/UpcomingMainLinksRequestUnitTests.cs
+++ Wykop.ApiProvider.UnitTests/Data/Link/Links/UpcomingMainLinksRequestUnitTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Net.Http;
 using NUnit.Framework;
-using RestSharp.Portable;
 using Wykop.ApiProvider.Data.LinkRequest.Links;
 using Wykop.ApiProvider.Data.Types;
 using Wykop.ApiProvider.UnitTests.Common;
@@ -10,35 +7,36 @@ using Wykop.ApiProvider.UnitTests.Common;
 namespace Wykop.ApiProvider.UnitTests.Data.Link.Links
 {
     [TestFixture]
-    public class PromotedMainLinksRequestUnitTests : LinksRequestTestFixture
+    public class UpcomingMainLinksRequestUnitTests : LinksRequestTestFixture
     {
         /* Method Parameters: none
          * API Parameters:
                appkey – klucz aplikacji
                page – strona
-	           sort – day (ostatnie 24 godz), week (ostatnie 7 dni), month (ostatnie 30 dni)
+               sort – day (ostatnie 24 godz), week (ostatnie 7 dni), month (ostatnie 30 dni)
          * POST Parameters: none
          */
 
-        private const int requestedPage = 2;
-        private readonly SortType sortType = SortType.CreateSortByDay();
-        private PromotedMainLinksRequest systemUnderTest;
+        private const int requestedPage = 3;
+        private readonly SortType sortType = SortType.CreateSortByMonth();
+        private UpcomingMainLinksRequest systemUnderTest;
 
         [SetUp]
         public void SetupTests()
         {
-            systemUnderTest = new PromotedMainLinksRequest()
+            systemUnderTest = new UpcomingMainLinksRequest()
             {
                 RequestedPage = requestedPage,
                 SortType = sortType
             };
 
-            ExpectedRequestUri = new Uri("http://a.wykop.pl/Links/Promoted/appkey," + UnitTestsConstants.AppKey + ",page,2,sort,day");
             LinksRequest = systemUnderTest;
+            ExpectedRequestUri = new Uri("http://a.wykop.pl/Links/Upcoming/appkey," + UnitTestsConstants.AppKey + 
+                                        ",page,3,sort,month");
         }
-  
+
         [Test]
-        public void BuildRestRequest_PostBodyShouldBeEmpty()
+        public void BuildRestRequest_ShouldHaveEmptyPostBody()
         {
             systemUnderTest.BuildRestRequest().AssertThatRequestHasEmptyPostBody();
         }
