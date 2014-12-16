914888c6bc70e4ea8100c7ad78c8e0bb68bc9708
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-16 23:16:15 +0100

    Added logged user header control.
    Added shared resources & boolean to visibilty converter.

---
+++ Wykop/Wykop.Shared/View/ValueConverters/BooleanToVisibiltiyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Wykop.View.ValueConverters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool) value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var currentVisibility = (Visibility) value;
+            return currentVisibility == Visibility.Visible;
+        }
+    }
+}
