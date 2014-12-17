cdf7bf50f22efed8de624cb9caa8b2ce8ec9e496
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-17 15:54:47 +0100

    Added default wykop data provider (for received data which is not collection of smthing..)

---
+++ Wykop.ApiProvider/DataProviders/DefaultWykopDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using RestSharp.Portable;
+using Wykop.ApiProvider.Common.Constants;
+using Wykop.ApiProvider.Data;
+
+namespace Wykop.ApiProvider.DataProviders
+{
+    public class DefaultWykopDataProvider<TRequest, TResult> : IWykopDataProvider<TRequest, TResult>
+        where TRequest : WykopRequestBase
+    {
+        private readonly RestClient _restClient;
+
+        public DefaultWykopDataProvider()
+        {
+            _restClient = new RestClient(ApiConstants.HostUrl);
+        }
+
+        public async Task<TResult> GetData(TRequest fromRequest, CancellationToken cancellationToken)
+        {
+            // TODO: Error handling
+            var restDataRequest = fromRequest.BuildRestRequest();
+            var requestResults = await _restClient.Execute<TResult>(restDataRequest, cancellationToken).ConfigureAwait(false);
+
+            return requestResults.Data;
+        }
+    }
+}
---
+++ Wykop.ApiProvider/DataProviders/IWykopDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Wykop.ApiProvider.Data;
+
+namespace Wykop.ApiProvider.DataProviders
+{
+    public interface IWykopDataProvider<in TRequest, TResult>
+        where TRequest : WykopRequestBase
+    {
+        Task<TResult> GetData(TRequest fromRequest, CancellationToken cancellationToken);
+    }
+}
