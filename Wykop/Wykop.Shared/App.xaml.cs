80ce65fdc4f1232173a0f54d8d40aa4640511227
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 17:19:41 +0100

    Conditional logic - charms are jsut for Windows build.

--- Wykop/Wykop.Shared/App.xaml.cs
+++ Wykop/Wykop.Shared/App.xaml.cs
@@ -18,7 +18,10 @@ using Windows.UI.Xaml.Navigation;
 using Wykop.Common;
 using Wykop.Common.Cache;
 using Wykop.Common.Strings;
+
+#if WINDOWS_APP
 using Wykop.View.Charms;
+#endif
 
 // The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
 
