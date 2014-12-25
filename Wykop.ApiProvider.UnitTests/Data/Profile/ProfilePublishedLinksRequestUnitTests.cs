c9fe3b7350089f2f5ca4a75c27826b9365982e64
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 22:33:46 +0100

    Added "Profile/Published" method implementation with unit tests.

---
+++ Wykop.ApiProvider.UnitTests/Data/Profile/ProfilePublishedLinksRequestUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Wykop.ApiProvider.Data.Link.Profile;
+
+namespace Wykop.ApiProvider.UnitTests.Data.Profile
+{
+    [TestFixture]
+    public class ProfilePublishedLinksRequestUnitTests : ProfileLinksRequestTestFixture
+    {
+        private ProfilePublishedLinksRequest systemUnderTest;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            systemUnderTest = new ProfilePublishedLinksRequest("testUsername")
+            {
+                RequestedPage = 123
+            };
+
+            string expectedUriString = UnitTestsConstants.WykopHostUrl + "Profile/Published/param1/" +
+                                       systemUnderTest.ProfileUsername + "/appkey," +
+                                       UnitTestsConstants.AppKey + ",page,123";
+            ExpectedRequestUri = new Uri(expectedUriString);
+            WykopRequest = systemUnderTest;
+        }
+    }
+}
---
+++ Wykop.ApiProvider/Data/Link/Profile/ProfilePublishedLinksRequest.cs
@@ -0,0 +1,27 @@
+﻿using Wykop.ApiProvider.Data.LinkRequest.Helpers;
+
+namespace Wykop.ApiProvider.Data.Link.Profile
+{
+    public class ProfilePublishedLinksRequest : ProfileLinksRequest
+    {
+        public ProfilePublishedLinksRequest(string userName) : base(userName)
+        {
+
+        }
+
+        public int RequestedPage { get; set; }
+
+        internal override string GetResourceMethodName()
+        {
+            return "Published";
+        }
+
+        protected override void BuildParameters()
+        {
+            base.BuildParameters();
+
+            AddApiParameterToRequest(ApiParameterProvider.GetApplicationKeyParameter());
+            AddApiParameterToRequest(ApiParameterProvider.GetPageParameter(RequestedPage));
+        }
+    }
+}
