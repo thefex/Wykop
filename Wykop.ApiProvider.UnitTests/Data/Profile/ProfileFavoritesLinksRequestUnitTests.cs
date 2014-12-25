937e9655b7c97060aa1bceb2f3930a3b5d9130cd
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 19:35:47 +0100

    Implemented "Profile/Favorites" method with unit tests.

---
+++ Wykop.ApiProvider.UnitTests/Data/Profile/ProfileFavoritesLinksRequestUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using NUnit.Framework;
+using Wykop.ApiProvider.Data.Link.Profile;
+
+namespace Wykop.ApiProvider.UnitTests.Data.Profile
+{
+    [TestFixture]
+    public class ProfileFavoritesLinksRequestUnitTests : ProfileLinksRequestTestFixture
+    {
+        private ProfileFavoritesLinksRequest systemUnderTests;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            systemUnderTests = new ProfileFavoritesLinksRequest("testUsername")
+            {
+                RequestedListId = 3,
+                RequestedPage = 123456
+            };
+
+            string expectedUriString = UnitTestsConstants.WykopHostUrl + "Profile/Favorites/param1/" +
+                                       systemUnderTests.ProfileUsername + "/param2/3" +
+                                       "/appkey," + UnitTestsConstants.AppKey + ",page,123456";
+            ExpectedRequestUri = new Uri(expectedUriString);
+            WykopRequest = systemUnderTests;
+        }
+
+        [Test]
+        public void BuildRestRequest_RequestShouldBeSendByGetMethod()
+        {
+            Assert.AreEqual(HttpMethod.Get, systemUnderTests.BuildRestRequest().Method);
+        }
+    }
+}
---
+++ Wykop.ApiProvider/Data/Link/Profile/ProfileFavoritesLinksRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Wykop.ApiProvider.Data.LinkRequest.Helpers;
+
+namespace Wykop.ApiProvider.Data.Link.Profile
+{
+    public class ProfileFavoritesLinksRequest : ProfileLinksRequest
+    {
+        public ProfileFavoritesLinksRequest(string userName) : base(userName)
+        {
+        }
+
+        internal override string GetResourceMethodName()
+        {
+            return "Favorites";
+        }
+
+        public int RequestedListId { get; set; }
+        public int RequestedPage { get; set; }
+
+        protected override void BuildParameters()
+        {
+            base.BuildParameters();
+
+            AddMethodParameterToRequest(new MethodParameter()
+            {
+                MethodName = "param2",
+                Value = RequestedListId.ToString()
+            });
+
+            AddApiParameterToRequest(ApiParameterProvider.GetApplicationKeyParameter());
+            AddApiParameterToRequest(ApiParameterProvider.GetPageParameter(RequestedPage));
+        }
+
+    }
+}
