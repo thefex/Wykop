c3d70df0b9ab5a302b7af80e851aa0b40a583d59
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 17:39:37 +0100

    Added exception to be thrown when called method couldn't be realized because user is not logged.

---
+++ Wykop.ApiProvider/Exceptions/UserIsNotLoggedInException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wykop.ApiProvider.Exceptions
+{
+    public class UserIsNotLoggedInException : InvalidOperationException
+    {
+        public UserIsNotLoggedInException()
+            : base("User is not logged in to perform this operation..")
+        {
+        }
+    }
+}
