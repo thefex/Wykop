8d370c66420b62f3ffdd493932b39a9fc95adde4
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 19:21:13 +0100

    Changed UserCommentedLinksRequest to ProfileCommentedLinksRequest. Implemented this method (Profile/Commented) with suitable

---
+++ Wykop.ApiProvider.UnitTests/Data/Profile/ProfileCommentedLinksRequestUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using NUnit.Framework;
+using Wykop.ApiProvider.Data.Link.Profile;
+
+namespace Wykop.ApiProvider.UnitTests.Data.Profile
+{
+    [TestFixture]
+    public class ProfileCommentedLinksRequestUnitTests : ProfileLinksRequestTestFixture
+    {
+        private ProfileCommentedLinksRequest systemUnderTest;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            systemUnderTest = new ProfileCommentedLinksRequest("testUsername")
+            {
+                RequestedPage = 4
+            };
+
+            string expectedUriString = UnitTestsConstants.WykopHostUrl + "Profile/Commented/param1/" +
+                                       systemUnderTest.ProfileUsername + "/appkey," + UnitTestsConstants.AppKey +
+                                       ",page,4";
+            ExpectedRequestUri = new Uri(expectedUriString);
+            WykopRequest = systemUnderTest;
+        }
+
+        [Test]
+        public void BuildRestRequest_AssertThatRequestHttpMethodIsGet()
+        {
+            Assert.AreEqual(HttpMethod.Get, systemUnderTest.BuildRestRequest().Method);
+        }
+    }
+}
---
+++ Wykop.ApiProvider/Data/Link/Profile/ProfileCommentedLinksRequest.cs
@@ -0,0 +1,26 @@
+﻿using Wykop.ApiProvider.Data.LinkRequest.Helpers;
+
+namespace Wykop.ApiProvider.Data.Link.Profile
+{
+    public class ProfileCommentedLinksRequest : ProfileLinksRequest
+    {
+        public ProfileCommentedLinksRequest(string userName) : base(userName)
+        {
+        }
+
+        public int RequestedPage { get; set; }
+
+        internal override string GetResourceMethodName()
+        {
+            return "Commented";
+        }
+
+        protected override void BuildParameters()
+        {
+            base.BuildParameters();
+
+            AddApiParameterToRequest(ApiParameterProvider.GetApplicationKeyParameter());
+            AddApiParameterToRequest(ApiParameterProvider.GetPageParameter(RequestedPage));
+        }
+    }
+}
