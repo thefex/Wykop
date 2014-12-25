349bd15ba5e3d97caa43e4259676eda93f3baea4
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 19:16:01 +0100

    Implemented (Profile/Burried method) ProfileBuriedLinksRequest with unti tests. Removed old UserBuriedLinksRequest

---
+++ Wykop.ApiProvider.UnitTests/Data/Profile/ProfileBuriedLinksRequestUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Wykop.ApiProvider.Data.Exceptions;
+using Wykop.ApiProvider.Data.Link.Profile;
+
+namespace Wykop.ApiProvider.UnitTests.Data.Profile
+{
+    [TestFixture]
+    public class ProfileBuriedLinksRequestUnitTests : ProfileLinksRequestTestFixture
+    {
+        private ProfileBuriedLinksRequest systemUnderTest;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            systemUnderTest = new ProfileBuriedLinksRequest("testUsername", "testUserKey")
+            {
+                RequestedPage = 3
+            };
+
+            string expectedUriString = UnitTestsConstants.WykopHostUrl + "Profile/Buried/param1/" +
+                                       systemUnderTest.ProfileUsername +
+                                       "/userkey," + systemUnderTest.UserKey + ",appkey," + UnitTestsConstants.AppKey +
+                                       ",page,3";
+            ExpectedRequestUri = new Uri(expectedUriString);
+            WykopRequest = systemUnderTest;
+        }
+
+        [Test]
+        public void BuildRestRequest_WhenUserKeyIsEmptyOrNull_RequestCouldNotBeBuildExceptionShouldBeThrown()
+        {   
+            Assert.Throws<RequestCouldNotBeBuildException>(() =>
+                new ProfileBuriedLinksRequest("testUsername", string.Empty).BuildRestRequest());
+        }
+    }
+}
---
+++ Wykop.ApiProvider/Data/Link/Profile/ProfileBuriedLinksRequest.cs
@@ -0,0 +1,33 @@
+﻿using Wykop.ApiProvider.Data.Exceptions;
+using Wykop.ApiProvider.Data.LinkRequest.Helpers;
+
+namespace Wykop.ApiProvider.Data.Link.Profile
+{
+    public class ProfileBuriedLinksRequest : ProfileLinksRequest
+    {
+        public ProfileBuriedLinksRequest(string userName, string userKey) : base(userName)
+        {
+            UserKey = userKey;
+        }
+
+        public string UserKey { get; private set; }
+        public int RequestedPage { get; set; }
+
+        internal override string GetResourceMethodName()
+        {
+            return "Buried";
+        }
+
+        protected override void BuildParameters()
+        {
+            base.BuildParameters();
+
+            if (string.IsNullOrEmpty(UserKey))
+                throw new RequestCouldNotBeBuildException("You cant request for profile buries without UserKey...");
+
+            AddApiParameterToRequest(ApiParameterProvider.GetUserKeyParameter(UserKey));
+            AddApiParameterToRequest(ApiParameterProvider.GetApplicationKeyParameter());
+            AddApiParameterToRequest(ApiParameterProvider.GetPageParameter(RequestedPage));
+        }
+    }
+}
