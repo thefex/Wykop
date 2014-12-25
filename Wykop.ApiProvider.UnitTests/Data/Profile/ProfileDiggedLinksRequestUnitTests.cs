2c541cfd152685a8f2ed96f6cc8220b62ad3124b
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-25 19:26:47 +0100

    Implemented "Profile/Digged" method with unit tests.

---
+++ Wykop.ApiProvider.UnitTests/Data/Profile/ProfileDiggedLinksRequestUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using NUnit.Framework;
+using Wykop.ApiProvider.Data.Link.Profile;
+
+namespace Wykop.ApiProvider.UnitTests.Data.Profile
+{
+    [TestFixture]
+    public class ProfileDiggedLinksRequestUnitTests : ProfileLinksRequestTestFixture
+    {
+        private ProfileDiggedLinksRequest systemUnderTest;
+
+        [SetUp]
+        public void SetupUnitTests()
+        {
+            systemUnderTest = new ProfileDiggedLinksRequest("testUsername")
+            {
+                RequestedPage = 1231
+            };
+
+            string expectedUriString = UnitTestsConstants.WykopHostUrl + "Profile/Digged/param1/" +
+                                       systemUnderTest.ProfileUsername + "/appkey," + UnitTestsConstants.AppKey +
+                                       ",page,1231";
+            ExpectedRequestUri = new Uri(expectedUriString);
+            WykopRequest = systemUnderTest;
+        }
+
+        [Test]
+        public void BuildRestRequest_AssertThatRequestIsSendByGetMethod()
+        {
+            Assert.AreEqual(HttpMethod.Get, systemUnderTest.BuildRestRequest().Method);
+        }
+    }
+}
---
+++ Wykop.ApiProvider/Data/Link/Profile/ProfileDiggedLinksRequest.cs
@@ -0,0 +1,26 @@
+﻿using Wykop.ApiProvider.Data.LinkRequest.Helpers;
+
+namespace Wykop.ApiProvider.Data.Link.Profile
+{
+    public class ProfileDiggedLinksRequest : ProfileLinksRequest
+    {
+        public ProfileDiggedLinksRequest(string userName) : base(userName)
+        {
+        }
+
+        public int RequestedPage { get; set; }
+
+        internal override string GetResourceMethodName()
+        {
+            return "Digged";
+        }
+
+        protected override void BuildParameters()
+        {
+            base.BuildParameters();
+
+            AddApiParameterToRequest(ApiParameterProvider.GetApplicationKeyParameter());
+            AddApiParameterToRequest(ApiParameterProvider.GetPageParameter(RequestedPage));
+        }
+    }
+}
