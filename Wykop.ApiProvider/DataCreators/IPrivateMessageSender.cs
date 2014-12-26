93d08706769efe2d5522721b4d117c81eca1c818
Przemysław Raciborski <przemyslawraciborski@gmail.com>
2014-12-26 17:49:48 +0100

    Added private message sender - although it doesn't implement error handling yet.

---
+++ Wykop.ApiProvider/DataCreators/IPrivateMessageSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Wykop.ApiProvider.Model.Operations;
+
+namespace Wykop.ApiProvider.DataCreators
+{
+    public interface IPrivateMessageSender
+    {
+        Task<PrivateMessageSendResult> SendMessage(PrivateMessageContent messageContent, CancellationToken cancellationToken);
+    }
+}
---
+++ Wykop.ApiProvider/DataCreators/PrivateMessageSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using RestSharp.Portable;
+using Wykop.ApiProvider.Common.Constants;
+using Wykop.ApiProvider.DataCreators.Requests.PM;
+using Wykop.ApiProvider.Login;
+using Wykop.ApiProvider.Model.Operations;
+
+namespace Wykop.ApiProvider.DataCreators
+{
+    public class PrivateMessageSender : IPrivateMessageSender
+    {
+        private readonly ILoginService _loginService;
+        private readonly RestClient _restClient;
+
+        public PrivateMessageSender(ILoginService loginService)
+        {
+            _loginService = loginService;
+            _restClient = new RestClient(ApiConstants.HostUrl);
+        }
+
+        public async Task<PrivateMessageSendResult> SendMessage(PrivateMessageContent messageContent,
+            CancellationToken cancellationToken)
+        {
+            var loggedUser = await _loginService.GetLoggedUser();
+
+            // todo: error handling
+            var sendMessageRestRequest = new SendMessageWykopRequest(messageContent).BuildRestRequest();
+            dynamic results = await _restClient.Execute<object>(sendMessageRestRequest, cancellationToken);
+
+            if (results == "true")
+                return PrivateMessageSendResult.Sended;
+
+            throw new NotImplementedException("error handling not implemented yet @ private message sender");
+        }
+    }
+}
